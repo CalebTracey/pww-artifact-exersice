c5c78148c97aaa72e40f9f6f41de5e9cf70b40ee
Caleb Tracey <caleb.tracey@gmail.com>
2021-09-26 15:23:58 -0400

    Changed main header text to semi-bold

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './fonts/Open_Sans/static/OpenSans/OpenSans-SemiBold.ttf';
 import './fonts/Roboto_Condensed/RobotoCondensed-Regular.ttf';
 import './fonts/Roboto_Slab/static/RobotoSlab-Regular.ttf';
 import './fonts/Roboto_Slab/static/RobotoSlab-Bold.ttf';
+import './fonts/Open_Sans/static/OpenSans/OpenSans-Italic.ttf';
+import './fonts/Roboto_Slab/static/RobotoSlab-SemiBold.ttf';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
