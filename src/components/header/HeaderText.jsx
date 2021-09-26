4801fd0fee568b3dd4ffc73189213e2c8e5c5714
Caleb Tracey <caleb.tracey@gmail.com>
2021-09-26 18:48:08 -0400

    Sub menu bug fix - removed view height margin

--- src/components/header/HeaderText.jsx
+++ src/components/header/HeaderText.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const HeaderText = () => (
   <div className="header-text-wrapper">
-    <text className="header-text">
+    <div className="header-text">
       South Carolina <br /> Revenue and Fiscal Affairs Office <br />
-    </text>
-    <text className="header-sub-text">
+    </div>
+    <div className="header-sub-text">
       <small>Transforming data into solutions for South Carolina</small>
-    </text>
+    </div>
   </div>
 );
 
